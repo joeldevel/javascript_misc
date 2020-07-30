047d2e1a6ffe957ae7d7b9d3a93f3b0b56cabfd8
joeldevel <christianjoel.developer@gmail.com>
2020-07-30 16:36:06 -0300

    starting work for mobile first

--- key_events/index.js
+++ key_events/index.js
@@ -1,1 +1,30 @@
-console.log('works');
+// btns keyCodes
+// 37 38 39 40  65  83
+// l  u  r  d  'a' 's'
+const testAction = document.querySelector('#action-placeholder');
+
+window.addEventListener('keyup', function (event){
+  console.log(event.keyCode);
+  let key = ''
+  switch (event.keyCode) {
+    case 37: key = 'left';
+      break;
+    case 38: key = 'up';
+      break;
+    case 39: key = 'right';
+      break;
+    case 40: key = 'down';
+      break;
+    case 65: key = 'A';
+      break;
+    case 83: key = 'S';
+      break;
+    default: key = ''
+  }
+  btns = document.querySelectorAll('.btn');
+  // console.log(btns);
+  testAction.textContent = key;
+    btns.forEach( btn => btn.classList.remove('btn-active'));
+  document.getElementById(key).classList.add('btn-active');
+  // console.log( key);
+})
